86f671011ade66b2a0a26cfe0d30b6879f14ef73
Clara Kim <02clara.kim@gmail.com>
2026-03-09 01:23:07 -0700

    add: 3 more student cards

--- backend/studentDB.py
+++ backend/studentDB.py
@@ -44,6 +44,51 @@ StudentDB = [
                 ["French III", 3.7], ["AP US History", 3.7], ["AP Macroeconomics", 4.0]
             ]
         }
+    },
+    {
+        "sid": 20253030,
+        "name": "Maya Rodriguez",
+        "level": "Freshman",
+        "prevClasses": {
+            "Freshman": [
+                ["English I", 4.0], ["Geometry", 3.7], ["Biology", 4.0], 
+                ["Spanish II", 3.3], ["Music", 4.0], ["AP Human Geography", 4.0]
+            ]
+        }
+    },
+    {
+        "sid": 20244040,
+        "name": "David Chen",
+        "level": "Sophomore",
+        "prevClasses": {
+            "Freshman": [
+                ["English I", 4.0], ["Algebra II", 3.7], ["Biology Honors", 3.7], 
+                ["Spanish I", 4.0], ["AP Computer Science Principles", 4.0], ["Music", 4.0]
+            ],
+            "Sophomore": [
+                ["English II", 3.7], ["Trigonometry", 3.3], ["Chemistry Honors", 3.0], 
+                ["AP World History", 3.7], ["Spanish II", 4.0], ["Journalism", 4.0]
+            ]
+        }
+    },
+    {
+        "sid": 20235050,
+        "name": "Alysa Taylor",
+        "level": "Junior",
+        "prevClasses": {
+            "Freshman": [
+                ["English I", 3.3], ["Algebra I", 3.0], ["Biology", 3.7], 
+                ["French I", 3.0], ["Music", 4.0], ["Theater", 4.0]
+            ],
+            "Sophomore": [
+                ["English II", 3.7], ["Geometry", 3.0], ["Chemistry", 3.3], 
+                ["French II", 3.0], ["AP European History", 3.7], ["Creative Writing", 4.0]
+            ],
+            "Junior": [
+                ["AP Language & Composition", 3.7], ["Algebra II", 3.3], ["Physics", 4.0], 
+                ["AP US History", 3.3], ["French III", 3.7], ["AP Psychology", 4.0]
+            ]
+        }
     }
     # Add more student data as needed
 ]
